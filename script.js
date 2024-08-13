caccac327f52799b66e6d0bb869536dcaa99e883
tauhid-topu <t.topu021@gmail.com>
2024-08-14 00:52:25 +0600

    details

---
+++ script.js
@@ -0,0 +1,63 @@
+// login button event handler 
+const loginBtn=document.getElementById("login");
+loginBtn.addEventListener("click",function(){
+    const loginArea= document.getElementById("login-area");
+    loginArea.style.display="none";
+    const transactionArea= document.getElementById("transaction-area");
+    transactionArea.style.display="block";
+});
+
+
+// deposit button event handler 
+const depositBtn= document.getElementById("addDeposit");
+depositBtn.addEventListener("click",function(){
+    const depositNumber=getInputNumber("depositAmount");
+    // const depositAmount=document.getElementById("depositAmount").value;
+    // const depositNumber= parseFloat(depositAmount);
+
+//     const currentDeposit = document.getElementById("currentDeposit").innerText;
+//    const currentDepositNumber=parseFloat(currentDeposit);
+//     const totalDeposit=depositNumber+currentDepositNumber;
+//     document.getElementById("currentDeposit").innerHTML=totalDeposit; 
+
+    updateSpanText("currentDeposit",depositNumber);
+
+    updateSpanText("currentBalance",depositNumber);
+
+    document.getElementById("depositAmount").value="";
+});
+
+// withdraw button handler 
+    const withdrawBtn=document.getElementById("addWithdraw");
+    withdrawBtn.addEventListener("click",function(){
+        const withdrawNumber=getInputNumber("withdrawAmount");
+        updateSpanText("currentWithdraw",withdrawNumber);
+        updateSpanText("currentBalance", -1*withdrawNumber)
+
+        document.getElementById("withdrawAmount").value="";
+    });
+
+function getInputNumber(id){
+    const Amount= document.getElementById(id).value;
+    const amountNumber=parseFloat(Amount);
+    return amountNumber;
+};
+
+function updateSpanText(id,depositNumber){
+    current=document.getElementById(id).innerText;
+    const currentNumber= parseFloat(current);
+    const total=depositNumber+currentNumber;
+    document.getElementById(id).innerText=total;
+};
+
+
+
+
+
+
+
+
+
+
+
+
